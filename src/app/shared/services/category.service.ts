7fdf1386adc4f9f2d6e15970bc139061929f0788
Matheus Oliveira <matheus.6148@gmail.com>
2026-02-07 19:21:38 -0300

    fix: correct unit and category update API requests

--- src/app/shared/services/category.service.ts
+++ src/app/shared/services/category.service.ts
@@ -27,7 +27,7 @@ export class CategoryService extends BaseService {
   }
 
   updateCategory(category: Category): Observable<Category> {
-    return this.http.put<Category>(this.apiUrl, category)
+    return this.http.put<Category>(`${this.apiUrl}/${category.id}`, { name: category.name })
       .pipe(catchError(this.handleError));
   }
 
