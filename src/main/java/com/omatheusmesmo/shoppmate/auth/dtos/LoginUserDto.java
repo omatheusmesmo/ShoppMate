f468df92ceef16b96d5d0611a23c3ba7870ba0df
Matheus Oliveira <matheus.6148@gmail.com>
2026-02-07 19:00:48 -0300

    refactor: move DTOs and exception handler to appropriate packages

---
+++ src/main/java/com/omatheusmesmo/shoppmate/auth/dtos/LoginUserDto.java
@@ -0,0 +1,11 @@
+package com.omatheusmesmo.shoppmate.auth.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginUserDto {
+    private String email;
+    private String password;
+}
--- src/main/java/com/omatheusmesmo/shoppmate/list/dtos/ShoppingListUpdateRequestDTO.java
+++ src/main/java/com/omatheusmesmo/shoppmate/list/dtos/ShoppingListUpdateRequestDTO.java
@@ -2,7 +2,7 @@ package com.omatheusmesmo.shoppmate.list.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record UpdateShoppingListRequestDTO(
+public record ShoppingListUpdateRequestDTO(
         @NotBlank(message = "List name cannot be blank")
         String name
 ) {
