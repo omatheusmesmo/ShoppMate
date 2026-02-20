f88bbf962899d4dd7eb1d0eaff85091080fcdfa7
Matheus André <92062874+matheusandre1@users.noreply.github.com>
2026-02-19 23:30:50 -0300

    fix: remove tap never used

--- frontend/src/app/shared/interceptors/auth.interceptor.ts
+++ frontend/src/app/shared/interceptors/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { catchError, tap } from 'rxjs';
+import { catchError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
