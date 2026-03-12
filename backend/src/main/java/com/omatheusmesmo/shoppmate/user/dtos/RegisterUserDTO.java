2d6f86f0a1032527698386d2a1d86f610f7475f8
Matheus Oliveira <matheus.6148@gmail.com>
2026-03-12 17:41:05 -0300

    fix(backend): fix fullName mapping in RegisterUserDTO

--- backend/src/main/java/com/omatheusmesmo/shoppmate/user/dtos/RegisterUserDTO.java
+++ backend/src/main/java/com/omatheusmesmo/shoppmate/user/dtos/RegisterUserDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Pattern;
 
 public record RegisterUserDTO(String email,
 
-        @JsonProperty("full_name") String fullName,
+        String fullName,
 
         @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).+$", message = "Password must contain at least one uppercase letter, one special character and a number!") String password) {
 }
