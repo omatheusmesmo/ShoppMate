c76d2fd86e3dcf3ddbd0e585a94f7b9cc15c96b3
SRokesh-28 <rokeshsadacharam@gmail.com>
2026-05-17 17:58:30 +0530

    Implement soft delete for Unit entity
    Added Hibernate soft delete support to the Unit entity.
    
    Changes:
    
    * Added @SQLDelete annotation to update the deleted flag instead of physically removing records
    * Added @where annotation to automatically filter out deleted records from queries
    * Enabled logical delete behavior for the units table

--- backend/src/main/java/com/omatheusmesmo/shoppmate/unit/entity/Unit.java
+++ backend/src/main/java/com/omatheusmesmo/shoppmate/unit/entity/Unit.java
@@ -6,8 +6,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import com.omatheusmesmo.shoppmate.shared.domain.DomainEntity;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
+@SQLDelete(sql = "UPDATE units SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
 @Table(name = "units")
 @Getter
 @Setter
