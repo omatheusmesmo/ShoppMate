9b692696bb51c475fb39f43056b57952c64bd4f0
Matheus André <matheusandr2@gmail.com>
2026-05-17 16:14:37 -0300

    feat: add system standard and owner properties to categories and units

--- backend/src/main/resources/db/migration/V2__CREATE_TABLE_UNITS.sql
+++ backend/src/main/resources/db/migration/V2__CREATE_TABLE_UNITS.sql
@@ -2,6 +2,8 @@ CREATE TABLE units (
     id BIGINT PRIMARY KEY,
     name VARCHAR(100) UNIQUE NOT NULL,
     symbol VARCHAR(20),
+    is_system_standard BOOLEAN NOT NULL DEFAULT FALSE,
+    owner_id BIGINT,
     created_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
     deleted BOOLEAN DEFAULT FALSE
@@ -9,3 +11,10 @@ CREATE TABLE units (
 
 CREATE UNIQUE INDEX idx_units_name ON units (name);
 CREATE INDEX idx_units_deleted ON units (deleted);
+
+ALTER TABLE units
+ADD CONSTRAINT fk_units_owner_id_users_id
+FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE SET NULL;
+
+CREATE INDEX idx_units_is_system_standard ON units (is_system_standard);
+CREATE INDEX idx_units_owner_id ON units (owner_id);
--- backend/src/main/resources/db/migration/V3__CREATE_TABLE_CATEGORIES.sql
+++ backend/src/main/resources/db/migration/V3__CREATE_TABLE_CATEGORIES.sql
@@ -1,6 +1,8 @@
 CREATE TABLE categories (
     id BIGINT PRIMARY KEY,
     name VARCHAR(100) UNIQUE NOT NULL,
+    is_system_standard BOOLEAN NOT NULL DEFAULT FALSE,
+    owner_id BIGINT,
     created_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
     deleted BOOLEAN DEFAULT FALSE
@@ -8,3 +10,10 @@ CREATE TABLE categories (
 
 CREATE UNIQUE INDEX idx_categories_name ON categories (name);
 CREATE INDEX idx_categories_deleted ON categories (deleted);
+
+ALTER TABLE categories
+ADD CONSTRAINT fk_categories_owner_id_users_id
+FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE SET NULL;
+
+CREATE INDEX idx_categories_is_system_standard ON categories (is_system_standard);
+CREATE INDEX idx_categories_owner_id ON categories (owner_id);
