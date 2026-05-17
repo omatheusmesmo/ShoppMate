c89557cb65ed13bf742d8155cfffd3b447a8e076
SRokesh-28 <rokeshsadacharam@gmail.com>
2026-05-17 18:03:35 +0530

    Implement soft delete for ShoppingList entity
    Added Hibernate soft delete support to the ShoppingList entity.
    
    Changes:
    
    * Added @SQLDelete annotation to update the deleted flag instead of physically removing records
    * Added @where annotation to automatically filter out deleted records from queries
    * Enabled logical delete behavior for the lists table

--- backend/src/main/java/com/omatheusmesmo/shoppmate/list/entity/ShoppingList.java
+++ backend/src/main/java/com/omatheusmesmo/shoppmate/list/entity/ShoppingList.java
@@ -16,8 +16,12 @@ import lombok.Setter;
 
 import com.omatheusmesmo.shoppmate.shared.domain.DomainEntity;
 import com.omatheusmesmo.shoppmate.user.entity.User;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
+@SQLDelete(sql = "UPDATE lists SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
 @Table(name = "lists")
 @Getter
 @Setter
