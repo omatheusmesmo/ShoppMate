287381ab55a8c69c0eb27f315c2fe75b38a326eb
SRokesh-28 <rokeshsadacharam@gmail.com>
2026-05-17 17:48:45 +0530

    Enable Spring Data JPA auditing
    Enabled Spring Data JPA auditing support in the application.
    
    Changes:
    
    * Added @EnableJpaAuditing to the main Spring Boot application class
    * Activated automatic auditing support for @CreatedDate and @LastModifiedDate annotations
    * Enabled automatic population of audit fields for entities extending BaseAuditableEntity

--- backend/src/main/java/com/omatheusmesmo/shoppmate/ShoppMateApplication.java
+++ backend/src/main/java/com/omatheusmesmo/shoppmate/ShoppMateApplication.java
@@ -2,9 +2,11 @@ package com.omatheusmesmo.shoppmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @RestController
 public class ShoppMateApplication {
 
